e9dd904dbc00e6e4d3b0d6b110246c14e331f6a8
Daantri <121228489+Daantri@users.noreply.github.com>
2022-12-22 12:43:56 +0100

    Update Pagina 3 - Checken.py

--- pages/Pagina 3 - Checken.py
+++ pages/Pagina 3 - Checken.py
@@ -190,9 +190,9 @@ if st.session_state.omloopplanning and st.session_state.datafile:
                         wenscount1 += 1
                 test4['stroomgebruik'][m] = start_capaciteit
                 lijst2.append(start_capaciteit/max_capacity_battery*100)
-        
+        st.session_state.test = test4
         start_percentage.append(lijst2)
-    st.session_state.test = test4
+   
     st.markdown(start_percentage)
     st.session_state.lijn_boven_capaciteit = lijn_boven_capaciteit
     st.session_state.lijn_onder_capaciteit = lijn_onder_capaciteit
