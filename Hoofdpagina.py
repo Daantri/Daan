58a7064d90670543f38c641aa00994f2eab43f48
Daantri <121228489+Daantri@users.noreply.github.com>
2022-12-23 11:43:44 +0100

    Update Hoofdpagina.py

--- Hoofdpagina.py
+++ Hoofdpagina.py
@@ -69,4 +69,6 @@ def displayPDF(file):
     pdf_display = F'<iframe src="data:application/pdf;base64,{base64_pdf}" width="700" height="1000" type="application/pdf"></iframe>'
 
     # Displaying File
-    st.markdown("Explanation tool", unsafe_allow_html=True)
+    st.markdown(pdf_display, unsafe_allow_html=True)
+    
+displayPDF("Explanation_tool.pdf")
